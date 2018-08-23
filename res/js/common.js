fadc51c39f49e104b4907949886ed4f4be23633c
xiaoxiaoqingyi <lideyi333@163.com>
2018-08-23 21:29:06 +0800

    update

--- res/js/common.js
+++ res/js/common.js
@@ -17,6 +17,12 @@ $(function () {
 
   // 复选框UI
   $('.check-box input').bind('click', function(){
+    var checkedLen = $('.check-box input:checked').length;
+    if(!checkedLen){
+      $('.table-do-batch').addClass('readonly');
+    }else{
+        $('.table-do-batch').removeClass('readonly');
+    }
     var $checkState = $(this).prop('checked');
     var $currentCheckbox = $(this).parents('.check-box');
     if($checkState){
